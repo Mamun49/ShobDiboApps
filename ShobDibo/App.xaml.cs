c95219522b77eb256ec330b17aee156f2bbd30db
mamun49 <ovijust@gmail.com>
2024-06-12 12:03:43 +0600

    Add project files.

---
+++ ShobDibo/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ShobDibo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ShobDibo/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ShobDibo
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
